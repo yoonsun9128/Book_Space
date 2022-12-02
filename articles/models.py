fec0fdfb7122ba49a7b009d28124b8910d13612a
koon <cjstmdgusqw@gmail.com>
2022-12-02 12:06:41 +0900

    [ADD] : articles.models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    #user = models.ForeignKey(User) 유저모델 생성후
+    title = models.TextField()
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
