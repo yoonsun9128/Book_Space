acab781b109a58feb1a35bf2f4279e3de92df70e
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-12-02 17:14:36 +0900

    [ADD]articles/models.py, urls.py, views.py, serializers.py 수정

--- articles/serializers.py
+++ articles/serializers.py
@@ -32,9 +32,15 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
         
         
-class ArticleCreateSerializer(serializers.ModelSerializer):
+
+class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = "__all__"
-        
-        
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer): # 게시글 작성 시리얼라이즈
+    class Meta:
+        model = Article
+        fields = ("title", "image", "content")
+
