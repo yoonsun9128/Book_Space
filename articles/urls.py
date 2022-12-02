0afaaa28422d2abda6164ec874e0f636585a1bae
koon <cjstmdgusqw@gmail.com>
2022-12-02 15:28:33 +0900

    test2

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from articles import views
 
 urlpatterns = [
+    path('', views.ArticleView.as_view(), name='article_view'),
     path("main/list/", views.ArticleListView.as_view(), name = "main_list")
 ]
