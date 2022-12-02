cc254fca47c9bbb4a143bbc99f2c28ed16730e6f
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-12-02 19:11:19 +0900

    [MOD]ThumBook/urls.py 수정

--- ThumBook/urls.py
+++ ThumBook/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("user/", include("user.urls")),
-    path("article/", include("articles.urls")),
+    path("users/", include("users.urls")),
+    path("articles/", include("articles.urls")),
 ]
