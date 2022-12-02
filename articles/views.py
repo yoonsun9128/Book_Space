b0fb9ac9677bc78c9f14bca46ee293ad6ba5445d
jangwon-seo <sjw887@naver.com>
2022-12-02 15:53:20 +0900

    [MOD] article.serializers.py, views.py 작성

--- articles/views.py
+++ articles/views.py
@@ -3,12 +3,20 @@ from rest_framework.generics import get_object_or_404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from articles.models import Article
-from articles.serializers import ArticleSerializer
+from articles.serializers import ArticleSerializer, ArticleCreateSerializer, ArticleCreateSerializer
 class ArticleView(APIView): #게시글 불러오기(인기글로)
     def get(self, request):
         popular_articles = Article.objects.all().order_by('likes')[:2]
         serializer = ArticleSerializer(popular_articles, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request): # 게시글 작성
+        serializer = ArticleCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)       
 
 
 
