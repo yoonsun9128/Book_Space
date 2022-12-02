86a7b1e529a3c809c10b7ffd0d762c8e46684c01
koon <cjstmdgusqw@gmail.com>
2022-12-02 18:15:17 +0900

    branch test

--- articles/urls.py
+++ articles/urls.py
@@ -7,5 +7,5 @@ from articles import views
 
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='article_view'),
-    path("main/list/", views.ArticleListView.as_view(), name = "main_list")
+    path("list/", views.ArticleListView.as_view(), name = "main_list")
 ]
