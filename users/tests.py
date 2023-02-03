362894800b4994a5201db65da7e62ee43bc41de9
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2023-02-03 12:05:36 +0900

    [ADD] users:tests.py 로그인 테스트코드 작성

--- users/tests.py
+++ users/tests.py
@@ -4,8 +4,9 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
-
+from users.models import User
 class UserSignUpAPIViewTestCase(APITestCase):
+    # 로그인 성공
     def test_signup(self):
         url = reverse('user:UserView')
         user_data = {
@@ -16,3 +17,17 @@ class UserSignUpAPIViewTestCase(APITestCase):
         }
         response = self.client.post(url,user_data)
         self.assertEqual(response.status_code, 201)
+
+class UserLoginAPIViewTestCode(APITestCase):
+    def setUp(self):
+        self.user_data = {
+            "email":"test@test.com",
+            "name":"test",
+            "password":"test123456"
+        }
+        self.user = User.objects.create_user('test@test.com','test','test123456')
+        print("확인",self.user.is_active) #True로 찍힘
+        # self.user.is_active = False
+    def test_login(self):
+        response = self.client.post(reverse('user:token_obtain_pair'), self.user_data)
+        self.assertEqual(response.status_code, 200)
